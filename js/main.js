e740bf907bf51e178c4b06cfb37cf11c873cce33
Jεan Sacren <sakiwit@gmail.com>
2018-06-12 01:53:17 -0600

    Add main.js javascript file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var customName = document.getElementById('customname')
+var randomize = document.querySelector('.randomize')
+var story = document.querySelector('.story')
+
+function randomValueFromArray (array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
