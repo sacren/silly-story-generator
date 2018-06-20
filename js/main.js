58e8f62a2b1c602e51d6c9b6fc3d3ca340f64ae9
Jεan Sacren <sakiwit@gmail.com>
2018-06-20 03:33:22 -0600

    Add jQuery() to delay javascript execution

--- js/main.js
+++ js/main.js
@@ -1,57 +1,59 @@
-var customName = document.getElementById('customname')
-var randomize = document.querySelector('.randomize')
-var story = document.querySelector('.story')
+jQuery(function ($) {
+  var customName = document.getElementById('customname')
+  var randomize = document.querySelector('.randomize')
+  var story = document.querySelector('.story')
 
-const storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+  const storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 
-const insertX = [
-  'Willy the Goblin',
-  'Big Daddy',
-  'Father Christmas'
-]
+  const insertX = [
+    'Willy the Goblin',
+    'Big Daddy',
+    'Father Christmas'
+  ]
 
-const insertY = [
-  'the soup kitchen',
-  'Disneyland',
-  'the White House'
-]
+  const insertY = [
+    'the soup kitchen',
+    'Disneyland',
+    'the White House'
+  ]
 
-const insertZ = [
-  'spontaneously combusted',
-  'melted into a puddle on the sidewalk',
-  'turned into a slug and crawled away'
-]
+  const insertZ = [
+    'spontaneously combusted',
+    'melted into a puddle on the sidewalk',
+    'turned into a slug and crawled away'
+  ]
 
-function randomValueFromArray (array) {
-  return array[Math.floor(Math.random() * array.length)]
-}
-
-randomize.addEventListener('click', result)
-
-function result () {
-  var newStory = storyText
-  var xItem = randomValueFromArray(insertX)
-  var yItem = randomValueFromArray(insertY)
-  var zItem = randomValueFromArray(insertZ)
-
-  newStory = newStory.replace(':insertx:', xItem)
-    .replace(':insertx:', xItem)
-    .replace(':inserty:', yItem)
-    .replace(':insertz:', zItem)
-
-  if (customName.value !== '') {
-    var name = customName.value
-
-    newStory = newStory.replace('Bob', name)
+  function randomValueFromArray (array) {
+    return array[Math.floor(Math.random() * array.length)]
   }
 
-  if (document.getElementById('uk').checked) {
-    var weight = Math.round(300 * 0.071429) + ' stone'
-    var temperature = Math.round((94 - 32) / 1.8) + ' centigrade'
+  randomize.addEventListener('click', result)
 
-    newStory = newStory.replace('94 farenheit', temperature).replace('300 pounds', weight)
+  function result () {
+    var newStory = storyText
+    var xItem = randomValueFromArray(insertX)
+    var yItem = randomValueFromArray(insertY)
+    var zItem = randomValueFromArray(insertZ)
+
+    newStory = newStory.replace(':insertx:', xItem)
+      .replace(':insertx:', xItem)
+      .replace(':inserty:', yItem)
+      .replace(':insertz:', zItem)
+
+    if (customName.value !== '') {
+      var name = customName.value
+
+      newStory = newStory.replace('Bob', name)
+    }
+
+    if (document.getElementById('uk').checked) {
+      var weight = Math.round(300 * 0.071429) + ' stone'
+      var temperature = Math.round((94 - 32) / 1.8) + ' centigrade'
+
+      newStory = newStory.replace('94 farenheit', temperature).replace('300 pounds', weight)
+    }
+
+    story.textContent = newStory
+    story.style.visibility = 'visible'
   }
-
-  story.textContent = newStory
-  story.style.visibility = 'visible'
-}
+})
