700a9390e03e18cc15d5647f622b68e36a52d48b
Jεan Sacren <sakiwit@gmail.com>
2018-06-22 01:58:28 -0600

    Add .css() for style manipulation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 jQuery(function ($) {
-  var story = document.querySelector('.story')
-
   const storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 
   const insertX = [
@@ -49,6 +47,6 @@ jQuery(function ($) {
     }
 
     $('.story').text(newStory)
-    story.style.visibility = 'visible'
+    $('.story').css('visibility', 'visible')
   })
 })
